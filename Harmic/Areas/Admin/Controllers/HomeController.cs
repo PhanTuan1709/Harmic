f5163aafbd74203bb85abaf923b1d8d9cbe930ed
PhanTuan1709 <phantuan20012018@gmail.com>
2024-12-05 08:00:13 +0700

    Add project files.

---
+++ Harmic/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Harmic.Areas.Admin.Controllers
+{
+
+	[Area("Admin")]
+
+
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
